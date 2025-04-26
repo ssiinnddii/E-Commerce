f39d755278fb5b16a44608867f4faa97b0ad2706
inviona <inviajna@gmail.com>
2025-04-26 16:53:05 +0200

    database setup

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 
 //routes
 import authRoutes from "./routes/auth.route.js";  
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -16,4 +17,6 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    
+    connectDB();
 });
