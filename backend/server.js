f12e22305b805d6f23c8210595060360d860c818
inviona <inviajna@gmail.com>
2025-05-03 20:23:38 +0200

    payment routes with stripe

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./lib/db.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
