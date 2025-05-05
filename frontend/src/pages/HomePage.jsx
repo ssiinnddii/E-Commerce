15514cff16c0d060dc674645acff81b3f152e8a7
inviona <inviajna@gmail.com>
2025-05-05 23:58:44 +0200

    featured component created

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
-import CategoryItem from '../components/CategoryItem'
+import { useEffect } from "react";
+import CategoryItem from "../components/CategoryItem";
+import { useProductStore } from "../stores/useProductStore";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 const categories = [
 	{ href: "/jeans", name: "Jeans", imageUrl: "/jeans.jpg" },
@@ -10,28 +12,33 @@ const categories = [
 	{ href: "/suits", name: "Suits", imageUrl: "/suits.jpg" },
 	{ href: "/bags", name: "Bags", imageUrl: "/bags.jpg" },
 ];
-const HomePage = () => {
-  return (
-    <div className='relative min-h-screen text-white overflow-hidden'>
-      <div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
-          <h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
-            Explore Our Categories
-          </h1>
-          <p className='text-center text-xl text-gray-300 mb-12'>
-            Discover the latest trends in eco-friendly fashion
-          </p>
 
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+const HomePage = () => {
+	const { fetchFeaturedProducts, products, isLoading } = useProductStore();
+
+	useEffect(() => {
+		fetchFeaturedProducts();
+	}, [fetchFeaturedProducts]);
+
+	return (
+		<div className='relative min-h-screen text-white overflow-hidden'>
+			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
+				<h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
+					Explore Our Categories
+				</h1>
+				<p className='text-center text-xl text-gray-300 mb-12'>
+					Discover the latest trends in eco-friendly fashion
+				</p>
+
+				<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
 					{categories.map((category) => (
-						<CategoryItem 
-              category={category}
-              key={category.name} />
+						<CategoryItem category={category} key={category.name} />
 					))}
 				</div>
-        </div>
 
-    </div>
-  )
-}
-
-export default HomePage
+				{!isLoading && products.length > 0 && <FeaturedProducts featuredProducts={products} />}
+			</div>
+		</div>
+	);
+};
+export default HomePage;
